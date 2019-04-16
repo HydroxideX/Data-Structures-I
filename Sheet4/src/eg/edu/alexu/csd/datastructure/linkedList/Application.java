fe4222055e824eb31fb51289bc228110feb8b271
Yahia <YahiaElsaadawy@gmail.com>
2019-04-16 02:22:31 +0200

    Auto stash before revert of "Layout"

---
+++ Sheet4/src/eg/edu/alexu/csd/datastructure/linkedList/Application.java
@@ -0,0 +1,26 @@
+package eg.edu.alexu.csd.datastructure.linkedList;
+import java.util.*;
+import java.awt.Point;
+public class Application implements IPolynomialSolver {
+    void setPolynomial(char poly, int[][] terms){
+
+    }
+    String print(char poly){
+
+    }
+    void clearPolynomial(char poly){
+
+    }
+    float evaluatePolynomial(char poly, float value){
+
+    }
+    int[][] add(char poly1, char poly2){
+
+    }
+    int[][] subtract(char poly1, char poly2){
+
+    }
+    int[][] multiply(char poly1, char poly2){
+
+    }
+}
---
+++ Sheet4/src/eg/edu/alexu/csd/datastructure/linkedList/SinglyLinkedList.java
@@ -0,0 +1,40 @@
+package eg.edu.alexu.csd.datastructure.linkedList;
+import java.util.*;
+import java.awt.Point;
+public class SinglyLinkedList implements ILinkedList {
+    private class Node{
+        private int Value;
+        private Node Next;
+    }
+    public Node Head;
+    public void add(int index, Object element) {
+
+    }
+    public void add(Object element);public Object get(int index){
+
+    }
+    public Object get(int index){
+
+    }
+    public void set(int index, Object element){
+
+    }
+    public void clear(){
+
+    }
+    public boolean isEmpty(){
+
+    }
+    public void remove(int index){
+
+    }
+    public int size(){
+
+    }
+    public ILinkedList sublist(int fromIndex, int toIndex){
+
+    }
+    public boolean contains(Object o){
+
+    }
+}
