fd8b2dcc4e660dc5313ea9a9675fb21a2c1168e8
Shehab Elkhaligy <41087771+Sirito97@users.noreply.github.com>
2019-04-20 23:31:57 +0200

    Fixed some bounds

--- Sheet4/src/eg/edu/alexu/csd/datastructure/linkedList/cs76_cs35/Main.java
+++ Sheet4/src/eg/edu/alexu/csd/datastructure/linkedList/cs76_cs35/Main.java
@@ -1,9 +1,9 @@
 package eg.edu.alexu.csd.datastructure.linkedList.cs76_cs35;
 
 
-import com.sun.org.apache.xpath.internal.SourceTree;
+//import com.sun.org.apache.xpath.internal.SourceTree;
 
-import javax.xml.soap.Node;
+//import javax.xml.soap.Node;
 import java.awt.*;
 import java.util.Scanner;
 
@@ -12,20 +12,18 @@ public class Main {
     public static Application newList = new Application();
     public static void main(String[] args) {
 
-
         int[][] array = new int[1][2];
         char poly;
         boolean end = false;
         boolean error;
         String input;
-
         while (!end) {
-            error=true;
+            error = true;
             printLayout();
             while (error) {
                 error = false;
                 input = scanner.next();
-                if (input.equals("1")||input.equals("2")||input.equals("3") ||input.equals("4") ||input.equals("5") ||input.equals("6") ||input.equals("7") ||input.equals("8") ) {
+                if (input.equals("1") || input.equals("2") || input.equals("3") || input.equals("4") || input.equals("5") || input.equals("6") || input.equals("7") || input.equals("8")) {
                     switch (input) {
                         case "1":
                             System.out.println("Insert the variable name: A, B or C");
@@ -33,66 +31,74 @@ public class Main {
                             System.out.println("Insert the polynomial terms in the form:\n(coeff1, exponent1), (coeff2, exponent2), ..");
                             array = getTerms();
                             newList.setPolynomial(poly,array);
-                            System.out.println("Polynomial "+poly+" is set");
+                            System.out.println("Polynomial " + poly + " is set");
                             break;
                         case "2":
-                            System.out.println("Enter the polynomial you want yo print");
-                            poly=getPoly();
-                            System.out.print("Polynomial "+poly + " =" +" " );
+                            System.out.println("Enter the polynomial you want To print");
+                            poly = getPolyForString();
+                            System.out.print("Polynomial " + poly + " =" + " ");
                             printPolynomial(poly);
                             System.out.println(" ");
                             break;
                         case "3":
                             System.out.println("Which polynomials you want to add ? ex A, B");
-                            char poly1=getPoly();
-                            char poly2=getPoly();
+                            char poly1 = getPoly();
+                            char poly2 = getPoly();
+                            if(newList.isEmpty())
                             newList.add(poly1,poly2);
+
                             break;
                         case "4":
                             System.out.println("Which polynomials you want to subtract ? ex A, B");
-                            char poly3=getPoly();
-                            char poly4=getPoly();
+                            char poly3 = getPoly();
+                            char poly4 = getPoly();
                             newList.subtract(poly3,poly4);
                             break;
                         case "5":
                             System.out.println("Which polynomials you want to multiply ? ex A, B");
-                            char poly5=getPoly();
-                            char poly6=getPoly();
+                            char poly5 = getPoly();
+                            char poly6 = getPoly();
                             int[][] array2 = newList.multiply(poly5,poly6);
-                            System.out.println(array2[0][0]);
-                            System.out.println(array2[0][1]);
-                            System.out.println(array2[1][0]);
-                            System.out.println(array2[1][1]);
-                            System.out.println(array2[2][0]);
-
                             break;
                         case "6":
+                            boolean error2=true;
                             System.out.println("Enter the polynomial");
-                            char poly7=getPoly();
-                            System.out.println("Enter the value");
-                            float value = scanner.nextFloat();
-                            newList.evaluatePolynomial(poly7,value);
+                            char poly7 = getPoly();
+                            while(error2) {
+                                error=false;
+                                System.out.println("Enter the value");
+                                String value = scanner.next();
+                                if (isNumeric(value)) {
+                                    float d = Float.parseFloat(value);
+                                    float result = newList.evaluatePolynomial(poly7, d);
+                                    System.out.println("Result is " + result);
+                                } else {
+                                    error=true;
+                                    System.out.println("Error value must be numerical only");
+                                }
+                            }
                             break;
                         case "7":
                             System.out.println("Enter the polynomial you want to clear");
-                            char poly8=getPoly();
+                            char poly8 = getPolyForString();
                             newList.clearPolynomial(poly8);
-                            System.out.println("Polynomial "+poly8+" cleared");
+                            System.out.println("Polynomial " + poly8 + " cleared");
                             break;
                         case "8":
                             System.out.println("Exit the program");
-                            end=true;
+                            end = true;
                             break;
                     }
 
                 } else {
                     System.out.println("Error >> choose from 1 to 8");
-                    error=true;
+                    error = true;
                 }
 
             }
         }
     }
+
     public static void printLayout() {
         System.out.println("Please choose an action");
         System.out.println("------------------------");
@@ -107,47 +113,70 @@ public class Main {
     }
 
     public static char getPoly() {
-        char poly = 0;
-        boolean error=true;
-        while(error) {
-            poly = scanner.next().charAt(0);
-            if(poly=='A'||poly=='B'||poly=='C')
-                error=false;
-            else {
+        String poly="Z";
+        boolean error = true;
+        while (error) {
+            poly = scanner.next();
+            if (poly.equals("A") || poly.equals("B") || poly.equals("C") ) {
+                error = false;
+            } else {
                 System.out.println("Error : Choose A,B or C only");
-                error=true;
+                error = true;
             }
         }
-        return poly;
+
+        return poly.charAt(0);
+    }
+
+    public static char getPolyForString() {
+        String poly = "Z";
+        boolean error = true;
+        while (error) {
+            poly = scanner.next();
+            if (poly.equals("A") || poly.equals("B") || poly.equals("C") || poly.equals("R")) {
+                error = false;
+            } else {
+                System.out.println("Error : Choose A,B,C or R only");
+                error = true;
+            }
+        }
+        return poly.charAt(0);
     }
 
     public static int[][] getTerms() {
-        System.out.println("Enter terms number");
-        int termsNumber=scanner.nextInt();
-        int[][] array = new int[termsNumber][2];
-        int check;
-        for(int i=0;i<termsNumber;i++) {
-            System.out.println("Enter term number "+(i+1) );
-                array[i][0] = scanner.nextInt();
-                array[i][1] = scanner.nextInt();
-
-//                    System.out.println("Do you want to add more ? (1)(0)");
-                   // check = scanner.nextInt();
-//                    if (check == 0)
-//                        return array;
-
-
+        boolean error=true;
+        while(error) {
+            System.out.println("Enter terms number");
+            String termsNumber = scanner.next();
+            if(isNumeric(termsNumber)) {
+                int[][] array = new int[Integer.parseInt(termsNumber)][2];
+                int check;
+                for (int i = 0;i < Integer.parseInt(termsNumber);i++) {
+                    System.out.println("Enter term number " + (i + 1));
+                    array[i][0] = scanner.nextInt();
+                    array[i][1] = scanner.nextInt();
+                }
+                error=false;
+                return array;
+            } else {
+                System.out.println("Error Choose Only Numbers !");
+                error=true;
+            }
 
         }
-        return array;
+
+        return null;
+    }
+    public static boolean isNumeric(String strNum) {
+        try {
+            double d = Double.parseDouble(strNum); // return the string in form of integer
+        } catch (NumberFormatException | NullPointerException nfe) {
+            return false;
+        }
+        return true;
     }
     public static void printPolynomial(char poly) {
         String a = newList.print(poly);
         System.out.print(a);
     }
-
 }
-
-
-}
-
